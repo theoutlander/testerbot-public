92c450c118b2acb0f5fbd76713b26ef9fe7f4a74
Nick Karnik <theoutlander@live.com>
2017-12-29 10:17:55 -0800

    Updated index file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ let jest = require('jest')
 
 console.log(__dirname)
 
-jest.runCLI({ onlyChanged: false, config: 'jest2.config.js' }, [path.resolve(path.basename(__dirname), '../', 'tests')], function (success) {
+jest.runCLI({ onlyChanged: false, config: './src/jest.config.js' }, [path.resolve(__dirname, 'runner')], function (success) {
   // console.log(success)
 })
