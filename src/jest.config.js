6332198da716015e54ca00413c8e7311cb008343
Nick Karnik <theoutlander@live.com>
2018-01-04 14:45:56 -0800

    Fixed issue where jest would skip all tests under node_modules Fixed an issue with concurrently and pkill missing dependencies Fixed CI failure due to SIGTERM from concurrently Updated README Fixed #97 Fixed #9

--- src/jest.config.js
+++ src/jest.config.js
@@ -5,6 +5,10 @@ module.exports = {
   globalTeardown: './teardown.js',
   testEnvironment: './puppeteer_environment.js',
   roots: ["runner"],
+  haste: {
+    providesModuleNodeModules: ["testerbot"]
+  },
+  testPathIgnorePatterns: [],
   globals: {
     TESTERBOT: {
       CONFIG: '../testerbot.config.js',
