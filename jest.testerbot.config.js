06c2487202b960716aabee890b5afd34ca9fc4e8
Nick Karnik <theoutlander@live.com>
2018-01-19 10:48:15 -0800

    Integrated jest-junit reporter Enabled code coverage Added separate jest config for testerbot code coverage run Ignoring cov data added test-cover cmd

---
+++ jest.testerbot.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  silent: false,
+  verbose: true,
+  globalSetup: './src/setup.js',
+  globalTeardown: './src/teardown.js',
+  testEnvironment: './src/puppeteer_environment.js',
+  testResultsProcessor: "jest-junit",
+  collectCoverage: true,
+  coverageDirectory: "<rootDir>/coverage",
+  roots: ["src/runner"],
+  haste: {
+    providesModuleNodeModules: ["testerbot"]
+  },
+  testPathIgnorePatterns: [],
+  globals: {
+    TESTERBOT: {
+      CONFIG: '../testerbot.config.js',
+    }
+  }
+}
