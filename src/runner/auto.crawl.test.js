497a6463d3a607dbc9e3a57719e6d98e8befd171
Nick Karnik <theoutlander@live.com>
2018-01-19 02:53:27 -0800

    ARgh 2!

--- src/runner/auto.crawl.test.js
+++ src/runner/auto.crawl.test.js
@@ -6,13 +6,20 @@ let runner = new Runner('../tests/__crawler__/auto')
 describe(`Automatic Crawl`, () => {
   let files = runner.getAllTestFiles()
 
-  // for each test file
-  files.forEach(filePath => {
-    // read file content
-    // console.log(filePath)
-    var testCase = require(filePath)
+  if (files) {
+    // for each test file
+    files.forEach(filePath => {
+      // read file content
+      // console.log(filePath)
+      var testCase = require(filePath)
 
-    // console.log(testCase)
-    runner.addAutoTest(testCase)
-  })
+      // console.log(testCase)
+      runner.addAutoTest(testCase)
+    })
+  }
+  else {
+    it('Preprocessor wasn\'t run', () => {
+      expect(true).toEqual(true)
+    })
+  }
 })
